4c61e4c7d09f0b24b04bbdb24e2aea636c7f0f0c
gongjun <speechtang@gmail.com>
2013-09-07 21:35:12 -0700

    D

---
+++ rcode_check_demo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*认证码验证
+*/
+
+require_once "renzhengbao.class.php";
+$sn="SQRE-2CBG-SQKN-7BT4";
+$rcode=$_GET['code'];
+$renzhengbao=new renzhengbao();
+$res=$renzhengbao->check_rcode($sn, $rcode);
+if($res) //认证码验证成功
+{
+
+	echo "succ";
+}
+else
+{
+
+	echo $renzhengbao->get_error_msg();
+}
